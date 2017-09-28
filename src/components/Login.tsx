183e75b2b70fbfcd84c60898520e7f95ea4d3fe9
Gian Carlo Vegas R <giancarlovr@gmail.com>
2017-09-27 20:13:04 -0500

    pagina de login maqueta HTML

---
+++ src/components/Login.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+export interface LoginProps {
+    compiler: string; 
+    framework: string;
+}
+
+export class Login extends React.Component<LoginProps, any> {
+    render() {
+        return <div>
+            
+        </div>
+    }
+}
