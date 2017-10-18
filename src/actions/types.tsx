2f176092e23ca5214d96d7bb9937fc8a3fc83d53
Gian Carlo Vegas R <giancarlovr@gmail.com>
2017-10-18 10:19:58 -0500

    delete unused files

--- src/actions/types.tsx
+++
@@ -1,7 +0,0 @@
-export const GOT_TOKEN = 'GOT_TOKEN'
-export const GOT_CUSTOMERS = 'GOT_CUSTOMERS';
-export const GOT_CUSTOMER = 'GOT_CUSTOMER';
-export const USER_CREATED = 'USER_CREATED';
-export const USER_UPDATED = 'USER_UPDATED';
-export const USER_DELETED = 'USER_DELETED';
-
